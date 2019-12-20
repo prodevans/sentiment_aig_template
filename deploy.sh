37402a4518162ff66cea545ea0e8daff49dedb6c
Ajay <ajay.prodevans@gmail.com>
2019-12-20 07:40:54 +0000

    changed title

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
 #cd /opt/app-root/src/sentiment_analysis
-echo "Deploying Sentiment Model........."
+echo "#####################################"
+echo "######Deploying Sentiment Model######"
+echo "#####################################"
 git add -A
 git config --global user.email "ajay.prodevans@gmail.com"
 git config --global user.name "Ajay"
 git commit -m "changed title"
 git push 
+echo "#############################################"
+echo "Please Enter Your Code Repository Credentials"
+echo "#############################################"
