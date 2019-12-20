61d5be22d5dd7e5ecb732167de2b34085865682b
Ajay <ajay.prodevans@gmail.com>
2019-12-20 08:51:20 +0000

    changed title

--- .ipynb_checkpoints/deploy-checkpoint.sh
+++ .ipynb_checkpoints/deploy-checkpoint.sh
@@ -2,10 +2,12 @@
 echo "#####################################"
 echo "######Deploying Sentiment Model######"
 echo "#####################################"
+echo "                             "
 git add -A
 git config --global user.email "ajay.prodevans@gmail.com"
 git config --global user.name "Ajay"
 git commit -m "changed title"
+echo "                             "
 echo "#############################################"
 echo "Please Enter Your Code Repository Credentials"
 echo "#############################################"
