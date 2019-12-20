d1de5620c7e819a4a9c8f42dd8a1e01803742709
Ajay Rudresh <47887498+Ajay2307@users.noreply.github.com>
2019-12-20 13:02:29 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#cd /opt/app-root/src/sentiment_analysis
+echo "Deploying Sentiment Model........."
+git add -A
+git config --global user.email "ajay.prodevans@gmail.com"
+git config --global user.name "Ajay"
+git commit -m "changed title"
+git push 
